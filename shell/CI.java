0a8af34285e9db732ec3d39949e2a4ab088ea59c
Andrzej Sieminski <andrzej.a.sieminski@gmail.com>
2016-07-19 00:06:30 +0200

    Prompt flag was added. Prompt method was updated. Other methods were added in CI. - dir - tree - cdUp - cdDown All commands are implemeted in main mehod.

--- shell/CI.java
+++ shell/CI.java
@@ -1,8 +1,12 @@
 package shell;
 
+import java.io.File;
+import java.io.IOException;
 
+//	Command Interpreter class.
 class CI {
 	private String prompt;
+	private Boolean promptFlag = false;
 	
 
 String getPrompt() {
@@ -12,31 +16,91 @@ String getPrompt() {
 	void setPrompt(String prompt) {
 		this.prompt = prompt;
 	}
+	
+Boolean getPromptFlag() {
+		return promptFlag;
+	}
 
 
-	//	Prompt method interprets command that starts with "prompt" and return adequate prompt value.
-	String prompt(String commandLine)
+//	Prompt method interprets command that starts with "prompt" and return adequate prompt value.
+	String prompt(String commandLine, String path)
 	{
 		if (commandLine.substring(7).equals("$cwd")) {
-			prompt = ("[MyShell] "+ System.getProperty("user.dir") + ">");
+			prompt = ("[MyShell] "+ path + ">");
+			this.promptFlag = true;
 			return prompt;
 		}
 		else if (commandLine.substring(7).equals("reset")) {
 			prompt = "[MyShell] $>";
+			this.promptFlag = false;
 			return prompt;
 		}
 		else {
 			prompt = ("[MyShell] " + commandLine.substring(7) +">");
+			this.promptFlag = false;
 			return prompt;
 		}
 	}
 	
 
-	//	Method that shows content of actual folder.
-	void dir() {
-		System.out.println("Zawartosc folderu: FILE|DIR");
-	
-	
+//	Method that shows content of actual folder. All files got prefix FILE and Directories have DIR.
+	void dir(String path) throws IOException {
+		File dirName = new File(path);
+		File[] dirContent = dirName.listFiles();
+		System.out.println(path);
+		for (File test : dirContent) 
+		{
+			
+			if (test.isFile())
+				System.out.println("FILE    " + test.getName());
+			else if (test.isDirectory())
+				System.out.println("DIR     " + test.getName());
+		}
+	}
+//	Method display folder structure of actual directory. Every level of directory path gets adequate amount of "-" prefix.
+	void tree(String path, String pref) throws IOException {
+		File dirName = new File(path);
+		File[] dirContent = dirName.listFiles();
+		for (File test : dirContent)
+		{
+			String prefix = pref+"-";
+			if (test.isDirectory()) 
+			{
+				System.out.println(prefix + test.getName());
+				tree(test.getPath(),prefix);
+			}
+		}
+	}
+//	Method use actual directory path and return new path to parent directory. If prompt was set using $cwd to 'working directory', then method set new temporary prompt.  Use getPrompt() to get updated prompt.
+	String cdUp(String path,Boolean promptFlag) throws IOException {
+		String newPath;
+		newPath = path.substring(0, path.lastIndexOf("\\"));
+		if (newPath.length()==2)
+			newPath = newPath+"\\";
+		if (promptFlag=true)
+			prompt = ("[MyShell] "+ newPath + ">");
+		return newPath;
+	}
+//	Method use typed directory, directory path, promptFlag and returns new directory path or null if typed directory doesn't exist. If prompt was set using $cwd to 'working directory', then method set new temporary prompt. Use getPrompt() to get updated prompt.
+	String cdDown(String dir, String path, Boolean promptFlag) throws IOException {
+		String newPath=null;
+		File dirName = new File(path);
+		File[] dirContent = dirName.listFiles();
+		for (File test : dirContent)
+		{
+			if (test.isDirectory() && dir.equals(test.getName()))
+			{
+				if (path.length()>3) 
+				{
+					newPath = path+ "\\"+ dir;
+				}
+				else newPath = path + dir;
+			}
+			
+		}
+		if (promptFlag=true)
+			prompt = ("[MyShell] "+ newPath + ">");
+		return newPath;
 	}
 	
 	
--- shell/MyShell.java
+++ shell/MyShell.java
@@ -1,5 +1,6 @@
 package shell;
 
+import java.io.IOException;
 import java.util.Scanner;
 import shell.CI;
 
@@ -7,6 +8,7 @@ public class MyShell {
 
 	static String commandLine;
 	static String shellPrompt;
+	static boolean shellPromptFlag = false;
 	
 private MyShell() {
 	shellPrompt = "[MyShell] $>";
@@ -23,36 +25,48 @@ private Scanner extracted() {
 }
 
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
 		MyShell myShell = new MyShell();
 		myShell.readCommandLine();
+		String path = System.getProperty("user.dir");
 		
 //		My Shell body.
 		CI cI = new CI();
 		while (!MyShell.commandLine.equals("exit")) {
+//			Set prompt. (prompt ..)
 			if (MyShell.commandLine.length()>6 && MyShell.commandLine.substring(0, 7).equals("prompt ")) 
 			{
-				shellPrompt = cI.prompt(MyShell.commandLine);
+				shellPrompt = cI.prompt(MyShell.commandLine, path);
+				MyShell.shellPromptFlag = cI.getPromptFlag();
 			}
-
+//			Show folder content. (dir)
 			else if (MyShell.commandLine.equals("dir")) 
 			{
-				cI.dir();
-System.out.println("DIRRRRRRR");
+				cI.dir(path);
 			}
+//			Show folder structure. (tree)
 			else if (MyShell.commandLine.equals("tree")) 
 			{
-System.out.println("TREEEEEE");
+				System.out.println(path.substring(path.lastIndexOf("\\")+1));
+				cI.tree(path,"");
 			}
+//			Change folder up (cd..)
 			else if (MyShell.commandLine.equals("cd..")) 
 			{
-System.out.println("CD.......");
+				path = cI.cdUp(path,MyShell.shellPromptFlag);
+				if (MyShell.shellPromptFlag == true)
+					MyShell.shellPrompt = cI.getPrompt();
 			}
+//			Change folder to typed directory (cd + TypedDirectory)
 			else if (MyShell.commandLine.length()>3 && MyShell.commandLine.substring(0, 3).equals("cd ")) 
 			{
-System.out.println("CD xxxxxxxx");
+				String tmp = cI.cdDown(MyShell.commandLine.substring(3), path, MyShell.shellPromptFlag);
+				if (tmp == null)
+					System.out.println("Folder does't exist.");
+				else path = tmp;
+				if (MyShell.shellPromptFlag == true)
+					MyShell.shellPrompt = cI.getPrompt();
 			}
 //			This part covers not known commands and display information : "unknown command"
 			else 
